b6cc8f58714e1c88d8b7c628543932e52ae74103
Moold <mooldhu@gmail.com>
2019-09-27 11:02:28 +0800

    Add an example about using user defined alignment pipeline

---
+++ doc/bwa.sh
@@ -0,0 +1,29 @@
+#Set input and parameters
+polished_round=2
+threads=20 
+read1=reads1_R1.fastq.gz
+read2=reads1_R1.fastq.gz
+input=input.genome.fa
+for i in {1..${round}}; do
+#step 1:
+	#index the genome file and do alignment
+	bwa index ${input};
+	bwa mem -t ${threads} ${input} ${read1} ${read2}|samtools view --threads 3 -F 0x4 -b - |samtools sort - -m 2g --threads 5 -o sgs.sort.bam;
+	#index bam and genome files
+	samtools index -@ ${threads} sgs.sort.bam;
+	samtools faidx ${input};
+	#polish genome file
+	python NextPolish/lib/nextPolish.py -g ${input} -t 1 -p ${threads} -s sgs.sort.bam > genome.polishtemp.fa;
+	input=genome.polishtemp.fa;
+#step2:
+	#index genome file and do alignment
+	bwa index ${input};
+	bwa mem -t ${threads} ${input} ${read1} ${read2}|samtools view --threads 3 -F 0x4 -b - |samtools sort - -m 2g --threads 5 -o sgs.sort.bam;
+	#index bam and genome files
+	samtools index -@ ${threads} sgs.sort.bam;
+	samtools faidx ${input};
+	#polish genome file
+	python NextPolish/lib/nextPolish.py -g ${input} -t 2 -p ${threads} -s sgs.sort.bam > genome.nextpolish.fa;
+	input=genome.nextpolish.fa;
+done;
+#Finally polished genome file: genome.nextpolish.fa
